f683330fbf7207988dec49503b5ba580f8be1f13
Brian Charles <bcharles41078@gmail.com>
2020-07-15 13:56:15 -0500

    running streak

--- array-drills.js
+++ array-drills.js
@@ -6,34 +6,71 @@ const Array = require("./array");
 const { get } = require("./memory");
 
 function main() {
-  Array.SIZE_RATIO = 3;
+    Array.SIZE_RATIO = 3;
 
-  // Create an instance of the Array class
-  let arr = new Array();
+    // Create an instance of the Array class
+    let arr = new Array();
 
-  // Add an item to the array
-  arr.push(3);
-  arr.push(5);
-  arr.push(15);
-  arr.push(19);
-  arr.push(45);
-  arr.push(10);
-  arr.pop();
-  arr.pop();
-  arr.pop();
-  console.log(arr);
-  for (let i = 0; i < arr.length; i) {
-    arr.remove(0);
-  }
-  console.log(arr);
-  arr.push("tauhida");
-  console.log(arr.get(0));
+    // Add an item to the array
+    arr.push(3);
+    arr.push(5);
+    arr.push(15);
+    arr.push(19);
+    arr.push(45);
+    arr.push(10);
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    console.log(arr);
+    for (let i = 0; i < arr.length; i) {
+        arr.remove(0);
+    }
+    console.log(arr);
+    arr.push("tauhida");
+    console.log(arr.get(0));
 }
 
-main();
+//main();
 
 function firstItem() {
-  return Array.get(0);
+    return Array.get(0);
 }
 
-firstItem();
+//firstItem();
+
+function URLify(url) {
+    let newURL = ''
+    for (let i = 0; i < url.length; i++) {
+        if (url[i] !== ' ') {
+            newURL = newURL + url[i];
+        } else {
+            newURL = newURL + '%20'
+        }
+    }
+    console.log(newURL)
+}
+
+// URLify('tauhida parveen')
+// URLify('www.thinkful.com /tauh ida parv een')
+
+function filterArray(Array){
+    let newArray = []
+    for (let i = 0; i < Array.length; i++) {
+        if (Array[i] > 4) {
+            newArray.push(Array[i]);
+        }
+    }
+    console.log(newArray)
+}
+
+//filterArray([1,8,4,5,-18,18,20,3,2])
+
+function maxSum (Array){
+    let sum = 0
+    for (let i=0;i<Array.length;i++){
+        sum = sum + Array[i]
+    }
+    console.log(sum)
+}
+
+//maxSum([4, 6, -8, 13, -12, 2])
